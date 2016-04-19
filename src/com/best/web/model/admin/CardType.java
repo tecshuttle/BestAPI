e2fd69b69affabc8cc40e4a1083d6d4832779621
tecshuttle <tecshuttle@gmail.com>
2016-04-19 12:15:14 +0800

    卡类型编辑

--- src/com/best/web/model/admin/CardType.java
+++ src/com/best/web/model/admin/CardType.java
@@ -3,10 +3,35 @@ package com.best.web.model.admin;
 public class CardType {
 
     private String id;
+    private String create;
+    private String create_date;
+    private String modifier;
+    private String modify_date;
+    private String active_flag;
     private String card_name;
     private String card_code;
+    private String card_type;
+    private String status;
+    private String company_id;
+    private String intro;
+    private String logo_img;
+    private String use_rule;
+    private String open_date;
+    private String close_date;
+    private String valid_date_begin;
+    private String valid_date_end;
     private String price;
-
+    private String package_total;
+    private String select_count;
+    private String allow_change_count;
+    private String card_no_length;
+    private String card_code_length;
+    private String card_no_prefix;
+    private String card_no_sn_length;
+    private String valid_date_type;
+    private String valid_date_value;
+    private String company_abbr;
+    private String city_select;
 
     public String getId() {
         return id;
@@ -14,6 +39,46 @@ public class CardType {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getCreate() {
+        return create;
+    }
+
+    public void setCreate(String value) {
+        this.create = value;
+    }
+
+    public String getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(String value) {
+        this.create_date = value;
+    }
+
+    public String getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(String value) {
+        this.modifier = value;
+    }
+
+    public String getModify_date() {
+        return modify_date;
+    }
+
+    public void setModify_date(String value) {
+        this.modify_date = value;
+    }
+
+    public String getActive_flag() {
+        return active_flag;
+    }
+
+    public void setActive_flag(String value) {
+        this.active_flag = value;
     }
 
     public String getCard_name() {
@@ -32,12 +97,180 @@ public class CardType {
         this.card_code = card_code;
     }
 
+    public String getCard_type() {
+        return card_type;
+    }
+
+    public void setCard_type(String value) {
+        this.card_type = value;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String value) {
+        this.status = value;
+    }
+
+    public String getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(String value) {
+        this.company_id = value;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String value) {
+        this.intro = value;
+    }
+
+    public String getLogo_img() {
+        return logo_img;
+    }
+
+    public void setLogo_img(String value) {
+        this.logo_img = value;
+    }
+
+    public String getUse_rule() {
+        return use_rule;
+    }
+
+    public void setUse_rule(String value) {
+        this.use_rule = value;
+    }
+
+    public String getOpen_date() {
+        return open_date;
+    }
+
+    public void setOpen_date(String value) {
+        this.open_date = value;
+    }
+
+    public String getClose_date() {
+        return close_date;
+    }
+
+    public void setClose_date(String value) {
+        this.close_date = value;
+    }
+
+    public String getValid_date_begin() {
+        return valid_date_begin;
+    }
+
+    public void setValid_date_begin(String value) {
+        this.valid_date_begin = value;
+    }
+
+    public String getValid_date_end() {
+        return valid_date_end;
+    }
+
+    public void setValid_date_end(String value) {
+        this.valid_date_end = value;
+    }
+
     public String getPrice() {
         return price;
     }
 
     public void setPrice(String price) {
         this.price = price;
+    }
+
+    public String getPackage_total() {
+        return package_total;
+    }
+
+    public void setPackage_total(String value) {
+        this.package_total = value;
+    }
+
+    public String getSelect_count() {
+        return select_count;
+    }
+
+    public void setSelect_count(String value) {
+        this.select_count = value;
+    }
+
+    public String getAllow_change_count() {
+        return allow_change_count;
+    }
+
+    public void setAllow_change_count(String value) {
+        this.allow_change_count = value;
+    }
+
+    public String getCard_no_length() {
+        return card_no_length;
+    }
+
+    public void setCard_no_length(String value) {
+        this.card_no_length = value;
+    }
+
+    public String getCard_code_length() {
+        return card_code_length;
+    }
+
+    public void setCard_code_length(String value) {
+        this.card_code_length = value;
+    }
+
+    public String getCard_no_prefix() {
+        return card_no_prefix;
+    }
+
+    public void setCard_no_prefix(String value) {
+        this.card_no_prefix = value;
+    }
+
+    public String getCard_no_sn_length() {
+        return card_no_sn_length;
+    }
+
+    public void setCard_no_sn_length(String value) {
+        this.card_no_sn_length = value;
+    }
+
+    public String getValid_date_type() {
+        return valid_date_type;
+    }
+
+    public void setValid_date_type(String value) {
+        this.valid_date_type = value;
+    }
+
+    public String getValid_date_value() {
+        return valid_date_value;
+    }
+
+    public void setValid_date_value(String value) {
+        this.valid_date_value = value;
+    }
+
+    public String getCompany_abbr() {
+        return company_abbr;
+    }
+
+    public void setCompany_abbr(String value) {
+        this.company_abbr = value;
+    }
+
+    public String getCity_select() {
+        return city_select;
+    }
+
+    public void setCity_select(String value) {
+        this.city_select = value;
     }
 }
 
